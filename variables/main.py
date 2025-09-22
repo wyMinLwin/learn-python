affa916e551a60a22bb80fbaccd417573143b442
wyMinLwin <waiyanminlwin421@gmail.com>
2025-09-22 21:58:22 +0700

    learn: variables

---
+++ variables/main.py
@@ -0,0 +1,20 @@
+# Variables
+
+# Strings
+name = "L"
+age = 21
+code_in = "Python"
+
+print(f"{age} years old {name} use {code_in}")
+
+# Intergers
+date = 4
+print(f"May the {date}th be with you")
+
+# Floats
+PI = 3.14
+print(f"PI = {PI}")
+
+# Boolean
+is_active = True
+print(f"Is this account active {is_active}")
