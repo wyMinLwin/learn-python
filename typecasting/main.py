5290a9f6c67103b431094837e392cd84f61da056
wyMinLwin <waiyanminlwin421@gmail.com>
2025-09-22 22:19:36 +0700

    learn: typecasting

---
+++ typecasting/main.py
@@ -0,0 +1,13 @@
+# Typecasting
+
+# str(), int(), float(), bool()
+
+name = "L"
+age = 21
+is_active = True
+gpa = 3.2
+
+print(bool(name))
+print(int(gpa))
+print(float(age))
+print(str(is_active) + " is string tho")
