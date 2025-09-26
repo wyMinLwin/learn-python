084f25b829e6e682fed10304d351785910c0b8d4
wyMinLwin <waiyanminlwin421@gmail.com>
2025-09-26 13:19:28 +0700

    learn: arithmetic & math

---
+++ arithmetic/main.py
@@ -0,0 +1,11 @@
+# Arithmetic Operators
+
+friends = 10
+
+print(f"friends = {friends}")
+print(f"friends + 1 = { friends + 1 }")
+print(f"friends - 1 = { friends - 1 }")
+print(f"friends * 3 = { friends * 3 }")
+print(f"friends / 3 = { friends / 3 }")
+print(f"friends ** 2 = { friends ** 2 }")
+print(f"friends % 3 = { friends % 3 }")
---
+++ math/main.py
@@ -0,0 +1,22 @@
+print(f"round(2.5) = {round(2.5)}")
+print(f"round(3.5) = {round(3.5)}")
+
+print(f"abs(1) = {abs(1)}")
+print(f"abs(-4) = {abs(-4)}")
+
+print(f"pow(2,5) = {pow(2,5)}")
+print(f"max(1,2,3,4) = {max(1,2,3,4)}")
+
+
+# Math
+import math
+print(f"PI = {math.pi}")
+print(f"e = {math.e}")
+print(f"math.sqrt(4) = {math.sqrt(4)}")
+print(f"math.ceil(3.9) = {math.ceil(3.9)}")
+print(f"math.floor(3.9) = {math.floor(3.9)}")
+
+
+radius = float(input("Enter the radius of circle: "))
+result = round(math.pi * radius * 2, 2)
+print(f"circuference = {result}")
