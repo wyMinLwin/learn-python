b1ad16da50fa37c662928aa25827ef26d3900e5f
wyMinLwin <waiyanminlwin421@gmail.com>
2025-11-12 01:49:00 +0700

    timer

--- countdown_timer/main.py
+++ countdown_timer/main.py
@@ -3,7 +3,10 @@ import time
 input_time = int(input("Enter time in seconds: "))
 
 for t in range(input_time, 0, -1):
-    print(t)
+    seconds = t %  60
+    minutes = int(t / 60) % 60
+    hours = int(t/ 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("Oop! Time's up!")
